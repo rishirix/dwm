ab6efb8220189e4f1a8abc45f32020ced8390599
Rishi <rishibhargawa47@gmail.com>
2026-09-22 13:47:17 +0530

    centerfloating window patch

--- dwm.c
+++ dwm.c
@@ -1105,6 +1105,8 @@ manage(Window w, XWindowAttributes *wa)
 	updatewindowtype(c);
 	updatesizehints(c);
 	updatewmhints(c);
+	c->x = c->mon->mx + (c->mon->mw - WIDTH(c)) / 2;
+	c->y = c->mon->my + (c->mon->mh - HEIGHT(c)) / 2;
 	updatemotifhints(c);
 	XSelectInput(dpy, w, EnterWindowMask|FocusChangeMask|PropertyChangeMask|StructureNotifyMask);
 	grabbuttons(c, 0);
